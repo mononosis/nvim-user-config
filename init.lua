cd737c569c9a14e28aa38e84ebe0af27f9d2bbf2
Your Name <you@example.com>
2023-11-21 07:14:27 +0000

    test

--- init.lua
+++ init.lua
@@ -184,8 +184,11 @@ cmp.setup.cmdline(':', {
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 --require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
 --	capabilities = capabilities
 --}
+if not vim.fn.has('gui_running') then
+  vim.o.t_Co = "256"
+end
+
